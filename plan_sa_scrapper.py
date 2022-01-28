4f55ba5a18e0ece4e4058c640fa1c13709519bdf
DhruvSat <43302075+DhruvSat@users.noreply.github.com>
2022-01-28 06:49:44 +0530

    bug: modifiy script

--- plan_sa_scrapper.py
+++ plan_sa_scrapper.py
@@ -1,12 +1,10 @@
 import csv
 import time
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # Chrome Drivers
-PROXY = "103.92.114.2:443"
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--proxy-server=%s' % PROXY)
 driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver_win32\chromedriver.exe")
 driver.maximize_window()
 
@@ -23,70 +21,104 @@ STATUS = "//*[@id='DAPR-content']/div/div[1]/span"
 APPLICATION_ID = "//*[@id='printablearea']/fieldset[2]/dl/div[1]/dd"
 APPLICATION_NUMBER = "//*[@id='printablearea']/fieldset[2]/dl/div[3]/dd"
 COUNCIL_AREA = "//*[@id='printablearea']/fieldset[3]/dl/div[2]/dd[1]"
-TITLE = "//*[@id='printablearea']/fieldset[4]/dl/div[1]/dd[1]"
-PLAN_PARCEL = "//*[@id='printablearea']/fieldset[4]/dl/div[2]/dd[1]"
+TITLE1 = "//*[@id='printablearea']/fieldset[4]/dl/div[1]/dd[1]"
+TITLE2 = "//*[@id='printablearea']/fieldset[4]/dl/div[1]/dd[2]"
+PLAN_PARCEL1 = "//*[@id='printablearea']/fieldset[4]/dl/div[2]/dd[1]"
+PLAN_PARCEL2 = "//*[@id='printablearea']/fieldset[4]/dl/div[2]/dd[2]"
+NEXT_BUTTON = "//*/ul[@class = 'pagination simple-pagination']/li/a[@class = 'page-link next']"
+SHOW_MORE = "//*[@id='DAPR-content']/div/div[2]/p"
 
 # CSV Header
-header = ['Applicant', 'Property Address', 'Description', 'Lodged', 'Direct Link', 'Name', 'Status', 'Description', 'Application ID', 'Application number', 'Property Address', 'Council Area', 'Title', 'Plan Parcel']
-
-# Main URL Load
-MAIN_URL = "https://plan.sa.gov.au/development_application_register"
-driver.get(MAIN_URL)
+header = ['Applicant', 'Property Address', 'Description', 'Lodged', 'Direct Link', 'Name', 'Status', 'Descrip', 'Application ID', 'Application number', 'Property Addr', 'Council Area', 'Title', 'Plan Parcel']
 
 # Global Variables
 count = 1
 dataList = []
 hrefList = []
 
-# Click on More Applications
-driver.find_element(By.XPATH, SEARCH_BUTTON).click()
-time.sleep(15)
+listOfLinks = [line.strip() for line in open('property_links.txt')]
+print(len(listOfLinks))
 
-# Click on Load 100 Per Page Applications
-driver.find_element(By.XPATH, LISTMORE).click()
-time.sleep(15)
-
-# Find URL's From Page 
-tableHref = driver.find_elements(By.XPATH, TBODY_HREF)
-for link in tableHref:
-    links = link.get_attribute('href')
-    hrefList.append(links)
+# links = ['https://plan.sa.gov.au/development_application_register#view-21035707-DAP', 'https://plan.sa.gov.au/development_application_register#view-21035173-DAP','https://plan.sa.gov.au/development_application_register#view-21035173-DAP']
 
 # Route Through Founded Links
-for url in hrefList:
+for url in listOfLinks:
     try: 
         # Route Through URL's
         driver.get(url)
         print(f"Getting data from link {count}: {url}")
-        time.sleep(5)
+        time.sleep(7)
+
+        # Click on Show More Description if available
+        try:
+            driver.find_element(By.XPATH, SHOW_MORE).click()
+            time.sleep(1)
+        except:
+            pass
 
         # Find Property Details
-        applicantName = driver.find_element(By.XPATH, APPLICANT).text
-        propertyAddress = driver.find_element(By.XPATH, PROPERTY_ADDRESS).text
-        description = driver.find_element(By.XPATH, DESCRIPTION).text
-        lodged = driver.find_element(By.XPATH, LODGED).text
-        status = driver.find_element(By.XPATH, STATUS).text
-        appli_ID = driver.find_element(By.XPATH, APPLICATION_ID).text
-        appliNumber = driver.find_element(By.XPATH, APPLICATION_NUMBER).text
-        councilArea = driver.find_element(By.XPATH, COUNCIL_AREA).text
-        title = driver.find_element(By.XPATH, TITLE).text
-        planParcel = driver.find_element(By.XPATH, PLAN_PARCEL).text
-
-        desc = description.replace("Description","").replace("Show more.","").replace("\n","")
+        try: 
+            applicantName = driver.find_element(By.XPATH, APPLICANT).text
+        except:
+            applicantName = ""
+        try:
+            propertyAddress = driver.find_element(By.XPATH, PROPERTY_ADDRESS).text
+        except:
+            propertyAddress = ""
+        try:
+            description = driver.find_element(By.XPATH, DESCRIPTION).text
+        except:
+            description = ""
+        try:
+            lodged = driver.find_element(By.XPATH, LODGED).text
+        except:
+            lodged = ""
+        try:
+            status = driver.find_element(By.XPATH, STATUS).text
+        except: 
+            status = ""
+        try: 
+            appli_ID = driver.find_element(By.XPATH, APPLICATION_ID).text
+        except:
+            appli_ID = ""
+        try:     
+            appliNumber = driver.find_element(By.XPATH, APPLICATION_NUMBER).text
+        except:
+            appliNumber = ""
+        try:
+            councilArea = driver.find_element(By.XPATH, COUNCIL_AREA).text
+        except:
+            councilArea = ""
+        try: 
+            title = driver.find_element(By.XPATH, TITLE1).text
+            if "" not in title and "-" not in title:
+                title = title
+            else:
+                title = driver.find_element(By.XPATH, TITLE2).text
+        except:
+            title = ""
+        try:
+            planParcel = driver.find_element(By.XPATH, PLAN_PARCEL1).text
+            if "" not in planParcel and "-" not in planParcel:
+                planParcel = planParcel
+            else:
+                planParcel = driver.find_element(By.XPATH, PLAN_PARCEL2).text
+        except:
+            planParcel = ""
 
         # Create Data Object
         data = {
                 'Applicant': applicantName,
                 'Property Address': propertyAddress,
-                'Description': desc,
+                'Description': description,
                 'Lodged': lodged,
                 'Direct Link': url,
                 'Name': propertyAddress,
                 'Status': status,
-                'Description': description,
+                'Descrip': description,
                 'Application ID': appli_ID,
                 'Application number': appliNumber,
-                'Property Address': propertyAddress,
+                'Property Addr': propertyAddress,
                 'Council Area': councilArea,
                 'Title': title,
                 'Plan Parcel': planParcel
@@ -95,9 +127,15 @@ for url in hrefList:
         # Append Data Object To List
         dataList.append(data)
         count += 1
+        dataObject = json.dumps(dataList)   
+            
+        # Saving Data into .json file
+        with open("Property1.json", "w") as outfile:
+            outfile.write(dataObject)
 
     except:
         print("LINK ERROR...")
+        count += 1
         continue
 
 # Writing Into Csv File
---
+++ property_link.py
@@ -0,0 +1,34 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Chrome Drivers
+driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver_win32\chromedriver.exe")
+driver.maximize_window()
+
+# X_PATHS
+SEARCH_BUTTON = "//*[@id='DAPR-content']/div/div[1]/p[3]/a[1]"
+LISTMORE = "//*[@id='loadmessage']/div[5]/div[1]/p/span[3]/a"
+NEXT_BUTTON = "//*/ul[@class = 'pagination simple-pagination']/li/a[@class = 'page-link next']"
+SHOW_MORE = "//*[@id='DAPR-content']/div/div[2]/p"
+TBODY_HREF = "//*[@id='dapr']/tbody/tr/td/a[1]"
+
+# Main URL Load
+MAIN_URL = "https://plan.sa.gov.au/development_application_register"
+driver.get(MAIN_URL)
+
+# Continue on user resposne
+user = input("Press 'y' or 'Y' To Continue: ")
+if user == "y":
+
+    # Find URL's From Multiple Page
+    for i in range (95):
+        print(i)
+        if i != 0 :
+            driver.find_element(By.XPATH, NEXT_BUTTON).click()
+        time.sleep(20)
+        tableHref = driver.find_elements(By.XPATH, TBODY_HREF)
+        for links in tableHref:
+            link = links.get_attribute('href')
+            with open("property_links.txt", "a") as f:
+                f.write(link+'\n')
